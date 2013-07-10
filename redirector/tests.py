58dd6a854677710584fa45eaaea7e3521e103d98
Justin Quick <justquick@gmail.com>
2013-07-09 21:53:27 -0400

    added tests for redirect middleware

--- redirector/tests.py
+++ redirector/tests.py
@@ -1,16 +1,63 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.sites.models import Site
+
+from .models import Redirect
+from .middleware import RedirectMiddleware
+
+# Mock get_absolute_url method
+setattr(Site, 'get_absolute_url', lambda site: 'http://example.com/')
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class MockRequest(object):
+    def __init__(self, path):
+        self.path = path
+
+    def get_full_path(self):
+        return self.path
+
+
+class MockResponse(object):
+    def __init__(self, status_code=200):
+        self.status_code = status_code
+
+
+class RedirectTest(TestCase):
+    """
+    Redirector tests to test isolated functionality of middleware
+    """
+    def setUp(self):
+        self.middleware = RedirectMiddleware()
+        self.site = Site.objects.get_current()
+        self.basic_redirect = Redirect.objects.create(site=self.site, from_url='/a', to_url='/b/')
+        self.object_redirect = Redirect.objects.create(site=self.site, from_url='/site/', content_object=self.site)
+        self.bad_redirect = Redirect.objects.create(site=self.site, from_url='/bad/')
+
+    def test_no_op(self):
+        mock_response = MockResponse()
+        response = self.middleware.process_response(MockRequest('/content/'), mock_response)
+        self.assertEqual(mock_response, response)
+
+    def test_basic_redirect(self):
+        response = self.middleware.process_response(MockRequest('/a'), MockResponse(404))
+        self.assertEqual(response.status_code, 301)
+        self.assertEqual(response['location'], '/b/')
+
+    def test_query_redirect(self):
+        response = self.middleware.process_response(MockRequest('/a?foo=bar'), MockResponse(404))
+        self.assertEqual(response.status_code, 301)
+        self.assertEqual(response['location'], '/b/?foo=bar')
+
+    def test_slash_redirect(self):
+        response = self.middleware.process_response(MockRequest('/a/'), MockResponse(404))
+        self.assertEqual(response.status_code, 301)
+        self.assertEqual(response['location'], '/b/')
+
+    def test_object_redirect(self):
+        response = self.middleware.process_response(MockRequest('/site/'), MockResponse(404))
+        self.assertEqual(response.status_code, 301)
+        self.assertEqual(response['location'], 'http://example.com/')
+
+    def test_bad_redirect(self):
+        response = self.middleware.process_response(MockRequest('/bad/'), MockResponse(404))
+        self.assertEqual(response.status_code, 410)
+

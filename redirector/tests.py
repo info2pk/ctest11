2dadec71a662f4805e523413bc26739dcc9d1c86
carlos persico <persicocarlos@gmail.com>
2013-07-10 16:48:04 -0300

    adding tests

--- redirector/tests.py
+++ redirector/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
 from .models import Redirect
@@ -6,6 +7,7 @@ from .middleware import RedirectMiddleware
 
 # Mock get_absolute_url method
 setattr(Site, 'get_absolute_url', lambda site: 'http://example.com/')
+setattr(User, 'get_absolute_url', lambda user: '/users/mockuser/')
 
 
 class MockRequest(object):
@@ -31,6 +33,13 @@ class RedirectTest(TestCase):
         self.basic_redirect = Redirect.objects.create(site=self.site, from_url='/a', to_url='/b/')
         self.object_redirect = Redirect.objects.create(site=self.site, from_url='/site/', content_object=self.site)
         self.bad_redirect = Redirect.objects.create(site=self.site, from_url='/bad/')
+        self.mock_user = User.objects.create(username='test_user',
+                                            first_name='test_user',
+                                            last_name='test_user',
+                                            email='test@test.com')
+        self.user_redirect = Redirect.objects.create(site=self.site,
+                                                    from_url='/usertest/',
+                                                    content_object=self.mock_user)
 
     def test_no_op(self):
         mock_response = MockResponse()
@@ -61,3 +70,26 @@ class RedirectTest(TestCase):
         response = self.middleware.process_response(MockRequest('/bad/'), MockResponse(404))
         self.assertEqual(response.status_code, 410)
 
+    def test_deleted_object_redirect(self):
+        # Make sure that the redirect works correctly the first time
+        response = self.middleware.process_response(MockRequest('/usertest/'),
+                                                    MockResponse(301))
+        self.assertEqual(response.status_code, 301)
+        self.mock_user.delete()
+        # Test that the redirect is Gone
+        response = self.middleware.process_response(MockRequest('/usertest/'),
+                                                    MockResponse(410))
+        self.assertEqual(response.status_code, 410)
+        # Make sure that the changelist is correctly displayed
+        response = self.client.get('/admin/redirector/redirect/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_redirect_unicode(self):
+        self.assertEqual(self.basic_redirect.__unicode__(), u'/a ---> /b/')
+
+    def test_object_redirect_unicode(self):
+        self.assertEqual(self.user_redirect.__unicode__(), u'/usertest/ ---> /users/mockuser/')
+
+    def test_object_deleted_redirect_unicode(self):
+        self.mock_user.delete()
+        self.assertEqual(self.user_redirect.__unicode__(), u'/usertest/ ---> None')
